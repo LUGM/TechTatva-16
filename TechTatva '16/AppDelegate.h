c6030a6c668aa949fe151e8a62d52676a272bfd7
ykl7 <yklal95@gmail.com>
2016-06-13 21:01:38 +0530

    initial commit

---
+++ TechTatva '16/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TechTatva '16
+//
+//  Created by YASH on 13/06/16.
+//  Copyright © 2016 YASH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
