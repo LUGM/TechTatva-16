1a68580b61ed7ad798d554bf9c8ee75e21de6262
Abhishek Vora <abhivora8@gmail.com>
2016-06-21 21:29:30 +0530

    Image load error

---
+++ InstagramViewCellTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  InstagramViewCellTableViewCell.h
+//  TechTatva '16
+//
+//  Created by Abhishek Vora on 20/06/16.
+//  Copyright © 2016 YASH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InstagramViewCellTableViewCell : UITableViewCell
+
+@end
---
+++ TechTatva '16/InstagramTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  InstagramTableViewCell.m
+//  TechTatva '16
+//
+//  Created by Abhishek Vora on 21/06/16.
+//  Copyright © 2016 YASH. All rights reserved.
+//
+
+#import "InstagramTableViewCell.h"
+
+@implementation InstagramTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
