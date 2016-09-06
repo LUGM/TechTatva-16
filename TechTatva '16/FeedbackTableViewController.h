3a99ba6649c19e91f4bd819eea8ec89eb24ecb20
Abhishek Vora <abhivora8@gmail.com>
2016-09-07 00:19:04 +0530

    Feedback layout ready

---
+++ TechTatva '16/FeedbackTableViewController.h
@@ -0,0 +1,19 @@
+//
+//  FeedbackTableViewController.h
+//  TechTatva '16
+//
+//  Created by Abhishek Vora on 06/09/16.
+//  Copyright © 2016 YASH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedbackTableViewController : UITableViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *categoryName;
+@property (strong, nonatomic) IBOutlet UILabel *eventName;
+@property (strong, nonatomic) IBOutlet UITextField *commentsTextField;
+@property (strong, nonatomic) IBOutlet UIButton *submitButton;
+- (IBAction)submitButton:(UIButton *)sender;
+
+@end
