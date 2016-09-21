314e553d09c20097b9cbd078130781b8298d990a
Manas <mdresswalla@gmail.com>
2016-09-21 20:05:47 +0530

    Action added to buttons in About Us controller

--- AboutUsTableViewController.m
+++ AboutUsTableViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "AboutUsTableViewController.h"
+#import "Reachability.h"
+#import "PrefixHeader.pch"
 
 @interface AboutUsTableViewController ()
 
@@ -14,7 +16,44 @@
 
 @end
 
-@implementation AboutUsTableViewController
+@implementation AboutUsTableViewController{
+    Reachability *reachability;
+}
+
+- (void)openURLWithString:(NSString *)URLString backupURLString:(NSString *)backupURLString {
+    reachability = [Reachability reachabilityForInternetConnection];
+    if (reachability.isReachable) {
+        if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:URLString]])
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:URLString]];
+        else
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:backupURLString]];
+    }
+    else {
+        SVHUD_FAILURE(@"No connection!");
+    }
+}
+
+
+- (IBAction)facebookAction:(id)sender {
+    [self openURLWithString:@"https://www.facebook.com/mitrevels/" backupURLString:@"https://www.facebook.com/mitrevels/"];
+}
+
+- (IBAction)twitterAction:(id)sender {
+    [self openURLWithString:@"twitter://user?screen_name=revelsmit/" backupURLString:@"https://www.twitter.com/revelsmit/"];
+}
+
+- (IBAction)instagramAction:(id)sender {
+    [self openURLWithString:@"instagram://user?username=revelsmit" backupURLString:@"https://www.instagram.com/revelsmit/"];
+}
+
+- (IBAction)youtubeAction:(id)sender {
+    [self openURLWithString:@"youtube://www.youtube.com/channel/UC9gwWd47a0q042qwEgutjWw" backupURLString:@"http://www.youtube.com/user/UC9gwWd47a0q042qwEgutjWw"];
+}
+
+- (IBAction)snapchatAction:(id)sender {
+    [self openURLWithString:@"snapchat://add/revelsmit" backupURLString:@"http://www.snapchat.com/add/revelsmit/"];
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
